29d8a49aee449aa6ed44eaa0a631d53db241e784
Robert <Robert1cabrera@gmail.com>
2023-06-20 00:48:37 +0000

    font fixed

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -5,7 +5,7 @@ import React from "react";
 //create your first component
 const Jumbotron = () => {
 	return (
-	<div className="jumbotron jumbotron-fluid m-5  bg-light" style={{padding:'10PX'}}>
+	<div className="jumbotron jumbotron-fluid bg-light">
 		<h1 className="display-4">A Warm Welcome!</h1>
 		<p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto totam laudantium ipsa, a non est esse nemo consectetur illum quaerat voluptas vel exercitationem eos atque iste beatae itaque ab rerum enim velit?</p>
 		<a className="btn btn-primary btn-lg" href="https://reactjs.org/" role="button">Call To Action</a>
